0f828c19367878547c152ca288635b4082794a42
Ajay Gour <theajaygour@gmail.com>
2026-02-28 22:41:01 +0530

    final

--- day-11/src/app.js
+++ day-11/src/app.js
@@ -2,7 +2,8 @@ const express = require("express")
 const app = express()
 const userModel = require("./models/notes.model")
 const authRouter = require("./routes/auth.routes")
-
+const cookieParser = require("cookie-parser")
+app.use(cookieParser()) 
 app.use(express.json())
 app.use("/api/auth" , authRouter)
 module.exports = app;
--- day-11/src/routes/auth.routes.js
+++ day-11/src/routes/auth.routes.js
@@ -1,31 +1,93 @@
 const express = require("express")
 
-
+const jwt = require("jsonwebtoken")
 
 const userModel = require("../models/notes.model")
 
 const authRouter = express.Router()
 
-authRouter.post("/register" ,async (req,res)=>{
-    const {email , name , password} = req.body
+const bcrypt = require("bcrypt");
 
-    const isUserAlreadExists = await userModel.findOne({email})
-    if(isUserAlreadExists) {
+const crypto = require("crypto")
+
+authRouter.post("/register", async (req, res) => {
+    const { email, name, password } = req.body
+
+    const isUserAlreadExists = await userModel.findOne({ email })
+    if (isUserAlreadExists) {
         return res.status(400).json({
-            message:"user already exists ."
+            message: "user already exists ."
         })
     }
 
+
+    // HASHING OF THE PAASSWORD 
+    const hash = crypto.createHash("md5").update(password).digest("hex")
+
     const user = await userModel.create({
-        email , name , password
+        email, name,  password : hash
     })
 
+    // token creation
+    const token = jwt.sign(
+        {
+            id: user._id,
+            email: user.email,
+        },
+        process.env.JWT_SECRET
+    )
+
+    res.cookie("jwt_token", token )
+
     res.status(201).json({
-        message:"user registered",
-        user
+        message: "user registered",
+        user,
+        token
     })
 })
 
 
+// checking cokies
+authRouter.post("/protected" , (req,res)=>{
+ console.log(req.cookies);
+ 
+})
+
+// lgin credentails
+authRouter.post("/login" , async(req , res)=>{
+    const {email , password} = req.body
+
+    const user = await userModel.findOne({email})
+
+    if(!user) {
+        return res.status(404).json({
+            message:' user does nor found with this email address'
+        })
+    }
+
+     const isPasswordMatched = user.password ===  crypto.createHash("md5").update(password).digest("hex")
+
+     if(!isPasswordMatched){
+          return res.status(404).json({
+            message:' invalid  password '
+        })
+     }
+
+
+     const token = jwt.sign(
+        {
+            id: user._id,
+        },
+        process.env.JWT_SECRET
+    )
+
+    res.cookie("jwt_token", token )
+
+    res.status(201).json({
+        message: "user loggerd in . . ",
+        user,
+        
+    }) 
+})
 
 module.exports = authRouter 
